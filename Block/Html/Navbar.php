aaf8b71e8e78ec98aa6760e6834e005e92d7caeb
Binayak <ghoshbinayak@gmail.com>
2017-05-16 02:09:42 +0530

    init

---
+++ Block/Html/Navbar.php
@@ -0,0 +1,378 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Binayak\ScratchMod\Block\Html;
+
+use Magento\Framework\DataObject\IdentityInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\Data\TreeFactory;
+use Magento\Framework\Data\Tree\Node;
+use Magento\Framework\Data\Tree\NodeFactory;
+use Magento\Theme\Block\Html\Topmenu;
+
+/**
+ * Html page top menu block
+ */
+class Navbar extends Template implements IdentityInterface
+{
+    /**
+     * Cache identities
+     *
+     * @var array
+     */
+    protected $identities = [];
+
+    /**
+     * Top menu data tree
+     *
+     * @var \Magento\Framework\Data\Tree\Node
+     */
+    protected $_menu;
+
+    /**
+     * Core registry
+     *
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @param Template\Context $context
+     * @param NodeFactory $nodeFactory
+     * @param TreeFactory $treeFactory
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        NodeFactory $nodeFactory,
+        TreeFactory $treeFactory,
+        Topmenu $topmenu,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+
+        /* Use the beforGetHtml plugin interceptor in  
+        Catalog module to populate the menu object*/
+        $topmenu->getHtml();
+
+        $this->_menu = $topmenu->getMenu();
+    }
+
+    /**
+     * Get block cache life time
+     *
+     * @return int
+     */
+    protected function getCacheLifetime()
+    {
+        return parent::getCacheLifetime() ?: 3600;
+    }
+
+    /**
+     * Get top menu html
+     *
+     * @param string $outermostClass
+     * @param string $childrenWrapClass
+     * @param int $limit
+     * @return string
+     */
+    public function getHtml($outermostClass = '', $childrenWrapClass = '', $limit = 0)
+    {
+        $html = $this->_getHtml($this->_menu, $childrenWrapClass, $limit);
+        return $html;
+    }
+
+    /**
+     * Count All Subnavigation Items
+     *
+     * @param \Magento\Backend\Model\Menu $items
+     * @return int
+     */
+    protected function _countItems($items)
+    {
+        $total = $items->count();
+        foreach ($items as $item) {
+            /** @var $item \Magento\Backend\Model\Menu\Item */
+            if ($item->hasChildren()) {
+                $total += $this->_countItems($item->getChildren());
+            }
+        }
+        return $total;
+    }
+
+    /**
+     * Building Array with Column Brake Stops
+     *
+     * @param \Magento\Backend\Model\Menu $items
+     * @param int $limit
+     * @return array|void
+     *
+     * @todo: Add Depth Level limit, and better logic for columns
+     */
+    protected function _columnBrake($items, $limit)
+    {
+        $total = $this->_countItems($items);
+        if ($total <= $limit) {
+            return;
+        }
+
+        $result[] = ['total' => $total, 'max' => (int)ceil($total / ceil($total / $limit))];
+
+        $count = 0;
+        $firstCol = true;
+
+        foreach ($items as $item) {
+            $place = $this->_countItems($item->getChildren()) + 1;
+            $count += $place;
+
+            if ($place >= $limit) {
+                $colbrake = !$firstCol;
+                $count = 0;
+            } elseif ($count >= $limit) {
+                $colbrake = !$firstCol;
+                $count = $place;
+            } else {
+                $colbrake = false;
+            }
+
+            $result[] = ['place' => $place, 'colbrake' => $colbrake];
+
+            $firstCol = false;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Add sub menu HTML code for current menu item
+     *
+     * @param \Magento\Framework\Data\Tree\Node $child
+     * @param string $childLevel
+     * @param string $childrenWrapClass
+     * @param int $limit
+     * @return string HTML code
+     */
+    protected function _addSubMenu($child, $childLevel, $childrenWrapClass, $limit)
+    {
+        $html = '';
+        if (!$child->hasChildren()) {
+            return $html;
+        }
+
+        $colStops = null;
+        if ($childLevel == 0 && $limit) {
+            $colStops = $this->_columnBrake($child->getChildren(), $limit);
+        }
+
+        $html .= '<ul class="level' . $childLevel . ' submenu">';
+        $html .= $this->_getHtml($child, $childrenWrapClass, $limit, $colStops);
+        $html .= '</ul>';
+
+        return $html;
+    }
+
+    /**
+     * Recursively generates top menu html from data that is specified in $menuTree
+     *
+     * @param \Magento\Framework\Data\Tree\Node $menuTree
+     * @param string $childrenWrapClass
+     * @param int $limit
+     * @param array $colBrakes
+     * @return string
+     *
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    protected function _getHtml(
+        \Magento\Framework\Data\Tree\Node $menuTree,
+        $childrenWrapClass,
+        $limit,
+        $colBrakes = []
+    ) {
+        $html = '';
+
+        $children = $menuTree->getChildren();
+        
+        foreach ($children as $child) {
+            $html .= '<li>'
+                    . '<a href="' . $child->getUrl() . '">'
+                    . $child->getName()
+                    . "</a></li>";
+        }
+
+        return $html;
+    }
+
+
+    /**
+     * Generates string with all attributes that should be present in menu item element
+     *
+     * @param \Magento\Framework\Data\Tree\Node $item
+     * @return string
+     */
+    protected function _getRenderedMenuItemAttributes(\Magento\Framework\Data\Tree\Node $item)
+    {
+        $html = '';
+        $attributes = $this->_getMenuItemAttributes($item);
+        foreach ($attributes as $attributeName => $attributeValue) {
+            $html .= ' ' . $attributeName . '="' . str_replace('"', '\"', $attributeValue) . '"';
+        }
+        return $html;
+    }
+
+    /**
+     * Returns array of menu item's attributes
+     *
+     * @param \Magento\Framework\Data\Tree\Node $item
+     * @return array
+     */
+    protected function _getMenuItemAttributes(\Magento\Framework\Data\Tree\Node $item)
+    {
+        $menuItemClasses = $this->_getMenuItemClasses($item);
+        return ['class' => implode(' ', $menuItemClasses)];
+    }
+
+    /**
+     * Returns array of menu item's classes
+     *
+     * @param \Magento\Framework\Data\Tree\Node $item
+     * @return array
+     */
+    protected function _getMenuItemClasses(\Magento\Framework\Data\Tree\Node $item)
+    {
+        $classes = [];
+
+        $classes[] = 'level' . $item->getLevel();
+        $classes[] = $item->getPositionClass();
+
+        if ($item->getIsFirst()) {
+            $classes[] = 'first';
+        }
+
+        if ($item->getIsActive()) {
+            $classes[] = 'active';
+        } elseif ($item->getHasActive()) {
+            $classes[] = 'has-active';
+        }
+
+        if ($item->getIsLast()) {
+            $classes[] = 'last';
+        }
+
+        if ($item->getClass()) {
+            $classes[] = $item->getClass();
+        }
+
+        if ($item->hasChildren()) {
+            $classes[] = 'parent';
+        }
+
+        return $classes;
+    }
+
+
+    /**
+     * Add identity
+     *
+     * @param array $identity
+     * @return void
+     */
+    public function addIdentity($identity)
+    {
+        if (!in_array($identity, $this->identities)) {
+            $this->identities[] = $identity;
+        }
+    }
+
+
+    /**
+     * Get identities
+     *
+     * @return array
+     */
+    public function getIdentities()
+    {
+        return $this->identities;
+    }
+
+
+    /**
+     * Get cache key informative items
+     *
+     * @return array
+     */
+    public function getCacheKeyInfo()
+    {
+        $keyInfo = parent::getCacheKeyInfo();
+        $keyInfo[] = $this->getUrl('*/*/*', ['_current' => true, '_query' => '']);
+        return $keyInfo;
+    }
+
+
+    /**
+     * Get tags array for saving cache
+     *
+     * @return array
+     */
+    protected function getCacheTags()
+    {
+        return array_merge(parent::getCacheTags(), $this->getIdentities());
+    }
+
+
+    /**
+     * Check if current url is url for home page
+     *
+     * @return bool
+     */
+    public function isHomePage()
+    {
+        $currentUrl = $this->getUrl('', ['_current' => true]);
+        $urlRewrite = $this->getUrl('*/*/*', ['_current' => true, '_use_rewrite' => true]);
+        return $currentUrl == $urlRewrite;
+    }
+
+
+    /**
+     * Get logo image URL
+     *
+     * @return string
+     */
+    public function getLogoSrc()
+    {
+        if (empty($this->_data['logo_src'])) {
+            $this->_data['logo_src'] = $this->_getLogoUrl();
+        }
+        return $this->_data['logo_src'];
+    }
+
+
+    /**
+     * Retrieve logo image URL
+     *
+     * @return string
+     */
+    protected function _getLogoUrl()
+    {
+        $folderName = \Magento\Config\Model\Config\Backend\Image\Logo::UPLOAD_DIR;
+        $storeLogoPath = $this->_scopeConfig->getValue(
+            'design/header/logo_src',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        $path = $folderName . '/' . $storeLogoPath;
+        $logoUrl = $this->_urlBuilder
+                ->getBaseUrl(['_type' => \Magento\Framework\UrlInterface::URL_TYPE_MEDIA]) . $path;
+
+        if ($storeLogoPath !== null && $this->_isFile($path)) {
+            $url = $logoUrl;
+        } elseif ($this->getLogoFile()) {
+            $url = $this->getViewFileUrl($this->getLogoFile());
+        } else {
+            $url = $this->getViewFileUrl('images/logo.svg');
+        }
+        return $url;
+    }
+
+}
